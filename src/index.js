d37bf03a4a2bc3dcbcdcf5ad96a2931f3933a0b0
afchine said <afzongho.code@gmail.com>
2021-07-16 01:14:04 +0200

    add searchMovies component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import SearchMovies from './searchMovie';
 
 class Main extends React.Component {
   render() {
     return (
       <div className="container">
       <h1 className="title">React movie Search</h1>
+      <SearchMovies/>
       </div>
     );
   }
---
+++ src/searchMovie.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function SearchMovies() {
+    return( 
+        <form>
+            <label className="label" htmlFor="query">Movie name</label>
+            <input className="input" type="text" name="query"
+            placeholder="i.e Jurassic Park"  />
+            <button className="button" type="submit"> Search</button>
+            
+        </form>
+    )
+}
